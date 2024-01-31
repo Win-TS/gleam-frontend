dc3dd8514284dc83a688352fa000b5e58197da6a
TheerapakG <theerapakg@gmail.com>
2024-01-31 18:50:44 +0700

    fix: correct href type on external link

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { Platform } from "react-native";
 
 export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, "href"> & { href: string },
+  props: Omit<React.ComponentProps<typeof Link>, "href"> & {
+    href: `${string}:${string}`;
+  },
 ) {
   return (
     <Link
       target="_blank"
       {...props}
-      // @ts-expect-error: External URLs are not typed.
       href={props.href}
       onPress={(e) => {
         if (Platform.OS !== "web") {
